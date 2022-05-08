06cfb4117ebf053bdd6f963fa9964058dae5a3eb
Leandro Rios <leand@nb01sis05.mimo.corp>
2022-05-08 16:47:32 -0300

    creater cat-item component and connected our cart dropdown with cartitems reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -5,8 +5,8 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
     if (existingCartItem) {
         return cartItems.map( cartItem =>
-            cartItem === cartItemToAdd.id 
-            ? {...cartItem, quantity: cartItem.quantity + 1}
+            cartItem.id === cartItemToAdd.id 
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
             : cartItem 
         )
     }
