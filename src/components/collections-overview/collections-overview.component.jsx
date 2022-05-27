629db331d34cfa250ee35888abde8c1d26e6faf9
Leandro Rios <leand@nb01sis05.mimo.corp>
2022-05-26 22:31:31 -0300

    implement code that pulls in our shop data from firestore and stores it the Spinner HOC

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'; 
+import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
-import './collections-overview.styles.scss';
+import { CollectionsOverviewContainer } from './collections-overview.styles';
 
-const CollectionsOverview =  ({ collections }) => (
-    <div className ='shop-page'> 
-        {collections.map(({ id, ...otherCollectionsProps }) => (
-            <CollectionPreview key={id} {...otherCollectionsProps} />
-        ))}
-    </div>
+const CollectionsOverview = ({ collections }) => (
+  <CollectionsOverviewContainer>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollectionsForPreview
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,30 +1,29 @@
-import React from "react";
-
-import StripeCheckot from 'react-stripe-checkout';
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100;
-    const publishablekey = 'pk_test_51L0x4EIjOhrtZ479aWyM3XnGg205wxWXfpFVxTWankMXuyPlemCzNlCnT9WpVVqHOz0z77EFmKBVo4vnh9l2Bzeo00vScBxNCq'
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51L0x4EIjOhrtZ479aWyM3XnGg205wxWXfpFVxTWankMXuyPlemCzNlCnT9WpVVqHOz0z77EFmKBVo4vnh9l2Bzeo00vScBxNCq';
 
-    const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
-    }
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
 
-    return (
-        <StripeCheckot
-            label='Pay Now'
-            name='CRWN Cloting Ltd.'
-            billingAddress
-            shippingAddress
-            image= 'https://svgshare.com/i/CUz.svg'
-            description={`Your total is $${price}`}
-            amount={priceForStripe}
-            panelLabel='Pay Now'
-            token={onToken}
-            stripeKey={publishablekey}
-        />
-    );
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
 
 export default StripeCheckoutButton;
